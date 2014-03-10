eca6338df9a86cc34f0088fdc097d92a90bba24e
Michele Catalano <michele@catalano.de>
2014-03-10 04:57:34 +0100

    more mac compatibility.

--- example/dogl.c
+++ example/dogl.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
 #include "../src/dog128.h"
 
-int main()
+void usage()
 {
+  printf("Usage: dogl.sh [ -h ] [ -c command ] [ -d data ]\n");
+}
+
+int main(int argc, char **argv)
+{
+  char *command, *data;
+  int  ch, with_command = 0, with_data = 0;
   char *buff = "Hallo";
+
+  while ((ch = getopt(argc, argv, "hc:d:")) != -1) {
+    switch (ch) {
+      case 'h':
+        usage();
+        return 0;
+        break;
+      case 'c':
+        command = optarg;
+	with_command = 1;
+        break;
+      case 'd':
+        data = optarg;
+	with_data = 1;
+        break;
+      default:
+        usage();
+        exit(1);
+	break;
+    }
+  }
+
+  if (with_command == 1 && with_data == 0) {
+    printf("ERROR Command need data\n");
+    usage();
+    exit(1);
+  }
 
   initFonts();
 
@@ -15,13 +51,18 @@ int main()
   clear();
 
   show();
-  setPenColor(0);
 
-  writeText(buff, 1, 1);
-  buff = "Alex!";
-  writeText(buff, 1, 27);
-  //rect(10,10,100,40, false);
-  //circle(50, 30, 15, false);
+  if (with_command == 0) {
+    writeText(buff, 1, 1);
+    buff = "Alex!";
+    writeText(buff, 1, 27);
+    //rect(10,10,100,40, false);
+    //circle(50, 30, 15, false);
+  } else {
+    if (strncmp(strtolower(command), 'line', 7) == 0) { 
+      
+    }
+  }
 
   show();
 
--- example/dogl.sh
+++ example/dogl.sh
@@ -1,1 +1,1 @@
-LD_LIBRARY_PATH=../src/:$LD_LIBRARY_PATH ./dogl
+LD_LIBRARY_PATH=../src/:$LD_LIBRARY_PATH ./dogl $@
